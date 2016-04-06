c8eab9855ce6e9c3a6814fd38e4562e79be8ce08
Szymon Sołtysiak <simon@doctorkinetic.nl>
2016-04-06 08:55:38 +0200

    Create the Mediator abstract class.

---
+++ UnityProject/Assets/Zenject/Source/Mediator/Mediator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Zenject
+{
+    public abstract class Mediator<V> : MonoBehaviour
+        where V : MonoBehaviour
+    {
+        protected V View { get; set; }
+    }
+}
